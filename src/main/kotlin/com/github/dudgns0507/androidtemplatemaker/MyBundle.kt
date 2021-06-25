01c41b81e20a804d293ed80c8b79e6a433812112
GitHub Action <action@github.com>
2021-06-25 15:58:08 +0000

    Template cleanup

--- src/main/kotlin/com/github/dudgns0507/androidtemplatemaker/MyBundle.kt
+++ src/main/kotlin/com/github/dudgns0507/androidtemplatemaker/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.dudgns0507.androidtemplatemaker
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/dudgns0507/androidtemplatemaker/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.dudgns0507.androidtemplatemaker.services
+
+import com.github.dudgns0507.androidtemplatemaker.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
